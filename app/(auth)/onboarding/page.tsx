850bd40a3f24762d55a39d3d526f9b7600fe2eb0
Goldie Tiara <goldietiara.acc@gmail.com>
2023-08-20 14:16:54 +0700

    fixed user data type error on onboarding page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -13,21 +13,13 @@ export default async function Page() {
   if (userInfo?.onboard) redirect("/");
 
   const userData = {
-    id: user?.id,
-    objectId: userInfo._id,
-    username: userInfo?.username || user?.username,
-    name: userInfo?.name || user.firstName || "",
-    bio: userInfo?.bio || "",
-    image: userInfo?.image || user?.imageUrl,
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
   };
-  // const userData = {
-  //   id: user?.id,
-  //   objectId: userInfo._id,
-  //   username: user?.username,
-  //   name: user.firstName || "",
-  //   bio: "",
-  //   image: user?.imageUrl,
-  // };
 
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
